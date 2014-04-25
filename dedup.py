85049bea4c621ebc1ac7d9bb63741af4c2fb7fe7
Andrey Zholos <aaz@q-fu.com>
2014-04-25 14:50:24 +0100

    bump version number and dates

--- dedup.py
+++ dedup.py
@@ -448,7 +448,7 @@ def main():
                     "contains. Files are compared by content, directories by "
                     "item names and content.",
         add_help_option=False)
-    parser.version = "dedup 0.3.1"
+    parser.version = "dedup 0.4"
     parser.add_option("-h", "--help", action="help",
                       help=optparse.SUPPRESS_HELP)
     parser.add_option("--version", action="version",
